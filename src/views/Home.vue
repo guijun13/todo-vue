84211de959b37f491e4e046326c4c9ccc2a80f18
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-28 21:07:42 -0300

    :sparkles: Add list 'tarefas' addons
    Add new 'tarefa' input and button and remove specific 'tarefa' by id

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
     <h1>Tarefas</h1>
+    <input v-model="titulo">
+    <button v-on:click="adicionarTarefa()">
+      Nova tarefa
+    </button>
     <ul>
-      <li>
-        Tarefa 1
-      </li>
-      <li>
-        Tarefa 2
-      </li>
-      <li>
-        Tarefa 3
-      </li>
-      <li>
-        Tarefa 4
+      <li v-for="tarefa in tarefas" :key="tarefa.id">
+        {{tarefa.titulo}}
+        <button v-on:click="concluirTarefa(tarefa)">X</button>
       </li>
     </ul>
   </div>
@@ -23,6 +19,30 @@
 
 export default {
   name: 'Home',
+  data: () => {
+    return {
+      titulo: '',
+      tarefas: [
+        { id: 1, titulo: 'Tarefa 1' },
+        { id: 2, titulo: 'Tarefa 2' },
+        { id: 3, titulo: 'Tarefa 3' },
+      ]
+    };
+  },
+  methods: {
+    adicionarTarefa(){
+      this.tarefas.push({
+        id: 4,
+        titulo: this.titulo
+      });
+      this.titulo='';
+    },
+    concluirTarefa(tarefa){
+      // const idx = this.tarefas.indexOf(tarefa);
+      // this.tarefas.splice(idx, 1);
+      this.tarefas = this.tarefas.filter(x => x !== tarefa);
+    }
+  },
   components: {
   }
 }
