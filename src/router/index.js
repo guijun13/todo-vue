e2df282c968b9ceeae7e05e26c3b5c4054a644d5
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-28 21:52:30 -0300

    :sparkles: Add supabase data and Login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import supabase from '../supabase-client';
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/about',
@@ -16,14 +18,27 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && supabase.auth.user() === null) {
+    next({ name: 'Login' });
+  } else {
+    next();
+  }
+});
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>Registro</h1>
+    <p>Email: <input v-model="email"></p>
+    <p>Senha: <input type="password" v-model="senha"></p>
+    <p>
+      <button v-on:click="cadastrar()">Cadastrar</button>
+      <button v-on:click="entrar()">Entrar</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import supabase from '../supabase-client';
+import router from '../router';
+
+export default {
+  name: 'Login',
+  data: () => {
+    return {
+      email: '',
+      senha: ''
+    };
+  },
+  methods: {
+    async cadastrar(){
+      try{
+        const res = await supabase.auth.signUp({
+          email: this.email,
+          password: this.senha
+        });
+
+        if(res.error){
+          console.log(res.error);
+        } else {
+          router.push('/');
+        }
+      } catch(err){
+        console.error(err);
+        alert('Não foi possivel cadastrar');
+      }
+    },
+    async entrar(){
+      try{
+        const res = await supabase.auth.signIn({
+          email: this.email,
+          password: this.senha
+        });
+
+        if(res.error){
+          console.log(res.error);
+        } else {
+          router.push('/');
+        }
+      } catch(err){
+        console.error(err);
+        alert('Não foi possivel cadastrar');
+      }
+    }
+  },
+  components: {}
+}
+</script>
